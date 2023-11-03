261d7344da6e2d17a8aae4deedd78c215168a38a
Shuliii <ricoharsono@gmail.com>
2023-11-03 15:31:03 +0800

    edit

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/test", (req, res) => {
   res.json({ message: "Hello World!" });
 });
 
+app.get("/test/more", (req, res) => {
+  res.json({ message: "This is another test case!" });
+});
+
 app.listen(PORT, () => {
   console.log(`The server it's running on ${PORT}`);
 });
