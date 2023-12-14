36b91ee07fd6c8ed21717fbd54f3f7ce55f02989
Rico <v_fharsono@tencent.com>
2023-12-14 17:24:26 +0800

    edit

--- util/db.js
+++ util/db.js
@@ -1,9 +1,16 @@
 const mysql = require("mysql");
 
+// const connection = mysql.createPool({
+//   host: "us-cdbr-east-06.cleardb.net",
+//   user: "be62820570045a",
+//   password: "af6180b5",
+//   database: "heroku_ed1b580caca1292",
+// });
+
 const connection = mysql.createPool({
-  host: "us-cdbr-east-06.cleardb.net",
-  user: "be62820570045a",
-  password: "af6180b5",
+  host: "database-1-instance-1.cdc3lepmhoww.ap-southeast-1.rds.amazonaws.com",
+  user: "admin",
+  password: "lQ9bylVYw6XYwAm2S7tR",
   database: "heroku_ed1b580caca1292",
 });
 
